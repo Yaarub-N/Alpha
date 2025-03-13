c4828e013b934558e464642fa778b49bbcef9fec
Yaarub Nassr <yaarub.nasser@yahoo.com>
2025-03-13 23:23:45 +0100

    Fixat permanent ignoreringsproblem med .vs, bin och obj

--- WebApp/obj/Debug/net9.0/WebApp.AssemblyInfo.cs
+++ WebApp/obj/Debug/net9.0/WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7abca7693703f10a6ea2990a3c35b706ea46a8b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cf58256d6c74efa73da64f08fcc680f914f0a2d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
