e499d9ad1cd0f6b1dc7f33f4371987b302e49a95
Yaarub Nassr <yaarub.nasser@yahoo.com>
2025-03-31 16:30:12 +0200

    Add .gitignore again

---
+++ Business/Models/LoginForm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Models
+{
+    public class LoginForm
+    {
+
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email", Prompt = "Enter email address")]
+        public string Email { get; set; } = null!;
+
+        [Display(Name = "Password", Prompt = "Enter your password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = null!;
+
+    }
+}
---
+++ Business/Models/Services/ClientService.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace Business.Models.Services
+{
+    public class ClientService
+    {
+        public async Task<int> CreateAsync(AddClientForm form)
+        {
+            try
+            {
+                //    if (await _clientRepository.ExistsAsync(x => x.ClientName == form.ClientName))
+                return 409;
+
+                return 200;
+            }
+            catch
+            {
+                return 500;
+            }
+        }
+    }
+}
